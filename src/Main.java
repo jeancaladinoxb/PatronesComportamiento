a45753e86faa402e29e57ac08d0c517f53f474ff
jeancarloladino <jeancarloladinosg@gmail.com>
2026-05-09 11:12:15 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Modelo/Patrones/Chainofresponsibiliti/AuditorIA.java
    #	src/Modelo/Patrones/Chainofresponsibiliti/AuditorMedico.java
    #	src/Modelo/Patrones/Chainofresponsibiliti/BaseHandler.java
    #	src/Modelo/Patrones/Chainofresponsibiliti/DirectorMedico.java
    #	src/Modelo/Patrones/Chainofresponsibiliti/Handler.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import Modelo.entidades.SolicitudMedica;
+import java.util.Scanner;
+
 public class Main{
 
-    public static void main(String[]args){
-
-  int num = 0;
+    public static void  main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String nombrePaciente = sc.nextLine();
+        System.out.println("Ingrese su edad: ");
+        int edadPaciente = sc.nextInt();
+        System.out.println("Ingrese la fehca de solicitud de autorización: ");
+        int fechaSolicitud = sc.nextInt();
+        System.out.println("Ingrese el nivel de complejidad (1: Alto, 2: Medio y 3: Bajo )");
+        int nivelComplejidad = sc.nextInt();
+        System.out.println("Ingrese el costo estimado según la taba de costos EPS: ");
+        double costoEstimado = sc.nextDouble();
 
     }
-
 }
--- src/Modelo/entidades/SolicitudMedica.java
+++ src/Modelo/entidades/SolicitudMedica.java
@@ -1,4 +1,35 @@
-package Modelo.Patrones.Chainofresponsibiliti.entidades;
+package Modelo.entidades;
 
 public class SolicitudMedica {
+
+    private String nombrePaciente;
+    private int fechaSolicitud;
+    private int edadPaciente;
+    private int nivelComplejidad;
+    private double costoEstimado;
+
+
+    public SolicitudMedica(String nombrePaciente,int fechaSolicitud, int edadPaciente, int nivelComplejidad, double costoEstimado) {
+        this.nombrePaciente = nombrePaciente;
+        this.fechaSolicitud = fechaSolicitud;
+        this.edadPaciente = edadPaciente;
+        this.nivelComplejidad = nivelComplejidad;
+        this.costoEstimado = costoEstimado;
+    }
+
+    public String getNombrePaciente() {
+        return nombrePaciente;
+    }
+
+    public int getEdadPaciente() {
+        return edadPaciente;
+    }
+
+    public int getNivelComplejidad() {
+        return nivelComplejidad;
+    }
+
+    public double getCostoEstimado() {
+        return costoEstimado;
+    }
 }
